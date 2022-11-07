48f281e43d7b5717f9e2325440ddef6215c43a9d
root <root@Jose.localdomain>
2022-11-08 00:39:37 +0100

    modificacion08

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | cut -d : -f 1 | sed -n "p;n" | sort -r | rev | tr '\n' ','
+cat /etc/passwd | grep -v '#' |sed -n "n;p" | cut -d : -f 1 | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | tr '\n' ' ' | sed "s/ /, /g" | sed 's/$/./' | tr -d '\n'
