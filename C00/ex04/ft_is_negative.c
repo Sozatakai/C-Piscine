9a72eba875bc545791539d745c5bb5e9f5ce063f
Sozatakai <117180773+Sozatakai@users.noreply.github.com>
2022-12-06 15:41:57 +0100

    Update ft_is_negative.c

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -1,14 +1,18 @@
 #include <unistd.h>
 
-void ft_is_negative(int n){
-	if(n >= '0'){
+void ft_is_negative(int n)
+{
+	if(n >= '0')
+	{
 		write(1, "P", 1);
-	}else{
+	}else
+	{
 		write(1, "N", 1);
 	}
 }
 
-int main(){
+int main()
+{
 	int num = -9;
 	ft_is_negative(num);
 	return 0;
